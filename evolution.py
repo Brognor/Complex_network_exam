b3ec8bde9db4fdcea2ec10418b07771f6038da26
Brognor <42617332+Brognor@users.noreply.github.com>
2021-12-05 15:53:14 +0100

    Add files via upload

---
+++ evolution.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import networkit as nt
+import numpy as np 
+"""Small library to implement Laplacian evolution"""
+def A(G):
+    return nx.adjacency.matrix(G) #A is initizalized as the adjacency matrix of the graph
+
+def L(G):
+    return nx.laplacian_matrix(G) #L is initizalized as the lapalcian matrix of the graph
+
+def D(G):
+    return L(G)+A (G)#D is the degree matrix
+
+def transition_matrix(G):
+    return (A(G)*np.linalg.inv(D(G)))
+
+def evolution(G,vec,n_step):
+    """This function realizes the diffusion of the vector vec on the graph for a number of step=n_step"""
+    for i in range(n_step):
+        vec=transition_matrix(G)*vec
+    return vec
+    
+
+
