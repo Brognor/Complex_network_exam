8a9aa6db6031bcf6bc3b26493ddb76e843a7bfb1
calca98 <90897536+calca98@users.noreply.github.com>
2021-12-14 15:59:21 +0100

    Update RicciDiffusion.py

--- RicciDiffusion.py
+++ RicciDiffusion.py
@@ -17,6 +17,7 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.ERROR)
 # load GraphRicciCuravture package
 from GraphRicciCurvature.OllivierRicci import OllivierRicci
 
+# this does not care if the curvature is positive or negative
 
 def RicciEvolve (G,vec,n_step):
    
@@ -45,3 +46,33 @@ def RicciEvolve (G,vec,n_step):
             print("mass is not conserved, something happened")
             break
 
+# this does
+
+def RicciEvolve_sign (G,vec,n_step):
+   
+    for t in range(n_step):
+        W = [0] * len(list(G.nodes))
+        for n1 in list(G.nodes):
+            for n2 in list(G.neighbors(n1)):
+               if(G_orc[n1][n2]["ricciCurvature"]<0):
+                  W[n1] += abs(G[n1][n2]["ricciCurvature"])
+    
+        norm_vec = [0] * len(vec)
+        vec_copy = vec
+        for i in list(G.nodes):
+            if W[i] != 0:
+                norm_vec[i] = vec[i]/W[i]
+    
+        for n1 in list(G.nodes):
+            for n2 in list(G.neighbors(n1)):
+                if(G_orc[n1][n2]["ricciCurvature"]<0):
+                  vec[n1] += abs(G[n1][n2]["ricciCurvature"]) * norm_vec[n2]
+                  vec[n2] -= abs(G[n1][n2]["ricciCurvature"]) * norm_vec[n2]
+        
+        check_parameter = abs( sum(vec)- sum(vec_copy) )/sum(vec)
+
+        if check_parameter < 10^(-4):
+            vec = vec_copy
+        else: 
+            print("mass is not conserved, something happened")
+            break
