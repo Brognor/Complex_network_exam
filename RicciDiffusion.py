24102aea921d2519a4c5de3f691be372bd1a46ae
calca98 <90897536+calca98@users.noreply.github.com>
2021-12-14 15:50:03 +0100

    Add files via upload

---
+++ RicciDiffusion.py
@@ -0,0 +1,46 @@
+# RicciDiffusion.py
+# diffusion considering also the curvature
+
+# the quantity which must be difused is 
+# represented as a vector 
+
+import math
+import importlib
+
+# matplotlib setting
+import matplotlib.pyplot as plt
+
+# to print logs in jupyter notebook
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.ERROR)
+
+# load GraphRicciCuravture package
+from GraphRicciCurvature.OllivierRicci import OllivierRicci
+
+
+def RicciEvolve (G,vec,n_step):
+    W = [0] * len(list(G.nodes))
+    for t in range(n_step):
+        for n1 in list(G.nodes):
+            for n2 in list(G.neighbors(n1)):
+                W[n1] += abs(G[n1][n2]["ricciCurvature"])
+    
+        norm_vec = [0] * len(vec)
+        vec_copy = vec
+        for i in list(G.nodes):
+            if W[i] != 0:
+                norm_vec[i] = vec[i]/W[i]
+    
+        for n1 in list(G.nodes):
+            for n2 in list(G.neighbors(n1)):
+                vec[n1] += abs(G[n1][n2]["ricciCurvature"]) * norm_vec[n2]
+                vec[n2] -= abs(G[n1][n2]["ricciCurvature"]) * norm_vec[n2]
+        
+        check_parameter = abs( sum(vec)- sum(vec_copy) )/sum(vec)
+
+        if check_parameter < 10^(-4):
+            vec = vec_copy
+        else: 
+            print("mass is not conserved, something happened")
+            break
+
