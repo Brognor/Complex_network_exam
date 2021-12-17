a7c1f358019e8cf2589bfdd6987ea62ae9b138fc
dominego <91283592+dominego@users.noreply.github.com>
2021-12-17 10:41:14 +0100

    Update RicciDiffusion.py

--- RicciDiffusion.py
+++ RicciDiffusion.py
@@ -76,3 +76,12 @@ def RicciEvolve_sign (G,vec,n_step):
         else: 
             print("mass is not conserved, something happened")
             break
+      return vec
+           
+         
+  def Evolution_Flow(G, vec, n_step):
+    for i in range(n_step):
+        vec = RicciEvolve_sign(G, vec, 1)
+        orc = OllivierRicci(G)
+        orc.compute_ricci_flow(1)
+        G = orc.G.copy()
