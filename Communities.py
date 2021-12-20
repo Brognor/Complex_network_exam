c2edcb63dcf7c8d830db782f175b12a6cc65af2e
dominego <91283592+dominego@users.noreply.github.com>
2021-12-20 17:59:19 +0100

    Create Communities.py

---
+++ Communities.py
@@ -0,0 +1,24 @@
+import scipy.linalg as linalg
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def Communities(Laplacian):
+    w, v = linalg.eig(Laplacian)
+    w = w.real
+    u = w
+    u.sort()
+    lam = np.where(w == u[1])
+
+    com_1 = []
+    com_2 = []
+    color_map = []
+
+    for i in range(len(G.nodes)):
+        if v[i][lam[0]] > 0:
+            com_1 = np.append(com_1, i)
+            color_map = np.append(color_map, "blue")
+        else:
+            com_2 = np.append(com_2, i)
+            color_map = np.append(color_map, "red")
+    return com_1, com_2, color_map
