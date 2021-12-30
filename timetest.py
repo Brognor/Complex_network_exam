1bf5d51a1936d7311f326cf2cccaf3d89366f3f3
Brognor <42617332+Brognor@users.noreply.github.com>
2021-12-30 19:00:34 +0100

    Update timetest.py

--- timetest.py
+++ timetest.py
@@ -5,8 +5,47 @@ import numpy as np
 from tqdm import tqdm
 import evolution as evo
 
-f = open("datatime_formanfgfricci.txt","w")
-for i in tqdm(range (2,503)):
+f = open("datatime_classic.txt","w")
+for i in tqdm(range (2,500)):
+    G=nx.complete_graph(i)
+    
+    vec=np.ones(i)
+    vec=evo.normalize(vec)
+
+    start=time.time()
+    
+
+    evo.evolution(G,vec,10)
+
+    end=time.time()
+
+    f.write(f"{i}\t{round((end-start),5)}\n")
+    
+f.close()
+
+
+f = open("datatime_ollivierricci.txt","w")
+for i in tqdm(range (2,250)):
+    G=nx.complete_graph(i)
+    
+    vec=np.ones(i)
+    vec=evo.normalize(vec)
+
+    start=time.time()
+    G=RD.preprocces_Ollivier(G)
+
+    RD.OllivierRicciEvolve_sign(G,vec,10)
+
+    end=time.time()
+
+    f.write(f"{i}\t{round((end-start),3)}\n")
+    
+f.close()
+
+
+
+f = open("datatime_formanricci.txt","w")
+for i in tqdm(range (2,500)):
     G=nx.complete_graph(i)
     
     vec=np.ones(i)
@@ -15,10 +54,10 @@ for i in tqdm(range (2,503)):
     start=time.time()
     G=RD.preprocces_Forman(G)
 
-    RD.FormanRicciEvolve_sign(G,vec,50)
+    RD.FormanRicciEvolve_sign(G,vec,10)
 
     end=time.time()
 
-    f.write(f"{i}\t{round((end-start),3)}\n")
+    f.write(f"{i}\t{round((end-start),4)}\n")
     
 f.close()
