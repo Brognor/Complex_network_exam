9d1f16e6fc4840f81d5df4483ce1ed2d589a4331
calca98 <90897536+calca98@users.noreply.github.com>
2022-02-08 19:33:36 +0100

    Update graph_from_csv.py

--- graph_from_csv.py
+++ graph_from_csv.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-Data = open("musae_git_edges.csv", "r")
+Data = open("example.csv", "r")
 next(Data, None)  # skip the first line in the input file
 Graphtype = nx.Graph()
 
